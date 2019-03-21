ae4a333258870a72368ea3ca4fa9670fb24a7c3d
jleleu42 <jlu59@live.fr>
2019-03-21 15:18:56 +0100

    Test 1

--- tests/units/helloworld.php
+++ tests/units/helloworld.php
@@ -7,6 +7,7 @@ use mageekguy\atoum;
 require_once 'atoum.phar';
 require_once __DIR__ . '/../../helloworld.php';
 
+
 /*
 class helloWorld
 {
@@ -18,13 +19,15 @@ class helloWorld
 
 class helloWorld extends atoum
 {
-   public function testSay()
-   {
-      $helloWorld = new helloWorld();
 
-      $this->assert
-         ->string($helloWorld->say())->isEqualTo('Hello World !')
-         //->string('Hello World !')->isEqualTo('Hello World !')
+
+   public function testSay(){
+      $this
+            // création d'une nouvelle instance de la classe à tester
+            ->given($this->newTestedInstance)
+               ->then
+                    ->string($this->testedInstance->say())
+                        ->isEqualTo('Hello World !')
       ;
    }
 }
